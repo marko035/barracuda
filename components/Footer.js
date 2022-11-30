27e0ef1be2180f4fc69178762213b6cfa7c8580d
marko <manikolic@transperfect.com>
2022-11-30 01:30:10 +0100

    change copyright color to gray

--- components/Footer.js
+++ components/Footer.js
@@ -46,15 +46,15 @@ function Footer({ darkBackground, goldFooter }) {
           </div>
         </div>
 
-        <div className="w-full absolute bottom-0 px-6 xl:px-32 text-gray-600 flex text-sm xl:text-2xl font-thin">
-          <div className="border-t-2 border-gray-600 h-32 flex items-start xl:items-center flex-1 flex-col xl:flex-row p-4">
+        <div className="w-full absolute bottom-0 px-6 xl:px-32 text-[gray] flex text-sm xl:text-2xl font-thin">
+          <div className="border-t-2 border-[gray] h-32 flex items-start xl:items-center flex-1 flex-col xl:flex-row p-4">
             <div className="flex items-center mr-2">
               <span className="text-3xl mr-1">&#169;</span> Copyright 2022.
             </div>
             Barracudagloves, Inc.
             <span className="xl:ml-2">All rights reserved.</span>
           </div>
-          <div className="border-t-2 border-gray-600 h-32 flex items-center flex justify-end gap-2">
+          <div className="border-t-2 border-[gray] h-32 flex items-center flex justify-end gap-2">
             <img src="/facebook.png" width={24} height={24} alt="Facebook" />
             <img src="/instagram.png" width={25} height={24} alt="Instagram" />
           </div>
